fb3669c7598741340f3c29a28c74e4d4b5bf2cad
João Paulo Carvalho <joaopaulo.carvalho@br.experian.com>
2023-01-16 17:18:55 -0300

    feat(consumer) - adicionando dados no __init__ do pacote (#2)

---
+++ i4cast-consumer/i4cast_consumer/__init__.py
@@ -0,0 +1,5 @@
+
+__author__ = 'João Paulo Carvalho <email@jjpaulo2.dev.br>'
+__version__ = '1.0.0'
+__title__ = 'i4cast Consumer'
+__repository__ = 'https://github.com/jjpaulo2/weather-data-consumer'
