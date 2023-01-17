75e0250729ae3989250fe8ca2d33b86f67e97166
João Paulo Carvalho <joaopaulo.carvalho@br.experian.com>
2023-01-16 21:24:57 -0300

    feat(shell) - script template (#3)

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+
+STATION=$1;
+ENVIRONMENTAL_TYPE=$2;
+
+run_i4cast_consumer() {
+    
+}
+
+verify_execution() {
+
+}
+
+compress_output_files() {
+
+}
